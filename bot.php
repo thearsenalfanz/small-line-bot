fd36c1bff4170375ccf5c4a1e9fb55434a443fd3
thearsenalfanz <sodanamkang@email.com>
2017-02-03 15:43:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,6 +10,14 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 
+		ob_start();
+		var_dump($event);
+		$txt = ob_get_clean();
+		$reply = $txt;
+		$messages = [
+		'type' => 'text',
+		'text' => $reply
+		];	
 
 		if ($event['type'] == 'message') {
 
@@ -97,36 +105,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
-		else
-		{
-			ob_start();
-			var_dump($event);
-			$txt = ob_get_clean();
-			$reply = $txt;
-			$messages = [
-			'type' => 'text',
-			'text' => $reply
-			];	
-			
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";			
-		}
 	}
 }
 echo "No error";
