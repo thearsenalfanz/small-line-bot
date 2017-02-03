11dee5d31087eac7741064651cb000e611b96a07
thearsenalfanz <sodanamkang@email.com>
2017-02-03 15:41:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,7 +9,7 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+
 
 		if ($event['type'] == 'message') {
 
@@ -65,9 +65,12 @@ if (!is_null($events['events'])) {
 				}
 
 			}
-			else
+			else if($event['message']['type'] == 'sticker')
 			{
-					$reply = "else";
+					ob_start();
+					var_dump($event);
+					$txt = ob_get_clean();
+					$reply = $txt;
 					$messages = [
 					'type' => 'text',
 					'text' => $reply
@@ -93,6 +96,36 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+		}
+		else
+		{
+			ob_start();
+			var_dump($event);
+			$txt = ob_get_clean();
+			$reply = $txt;
+			$messages = [
+			'type' => 'text',
+			'text' => $reply
+			];	
+			
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";			
 		}
 	}
 }
