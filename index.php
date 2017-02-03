5ab6a956218e069c05fa2b64bb113c1d19373335
thearsenalfanz <sodanamkang@email.com>
2017-02-03 09:13:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "This is basis team LINE bot.";
-echo '<br><br><img src="images/CPU.png"></img>';
-echo '<br><br><img src="images/USR.png"></img>';
-echo '<br><br><img src="images/WP.png"></img>';
+echo '<br><br><img src="./images/CPU.png"></img>';
+echo '<br><br><img src="./images/USR.png"></img>';
+echo '<br><br><img src="./images/WP.png"></img>';
