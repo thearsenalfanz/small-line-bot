5aec20a51b7a3439059617953d52d1f868a8100b
thearsenalfanz <sodanamkang@email.com>
2017-02-06 09:05:13 +0700

    edit push msg

--- bot.php
+++ bot.php
@@ -100,25 +100,28 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-					$messages = [					
-					'type' => 'image',
-					'originalContentUrl' => 'https://basis-line-bot.herokuapp.com/images/WP.png',
-					'previewImageUrl'=> 'https://basis-line-bot.herokuapp.com/images/WP.png'
-					];
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-				'to' => 'Ue39b5f714f4424cb448ba4f6550bda5c',
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
+if (isset($_GET['push']) && $_GET['push'] = 1)
+{
+	$messages = [
+	'type' => 'image',
+	'originalContentUrl' => 'https://basis-line-bot.herokuapp.com/images/WP.png',
+	'previewImageUrl'=> 'https://basis-line-bot.herokuapp.com/images/WP.png'
+	];
+	$url = 'https://api.line.me/v2/bot/message/push';
+	$data = [
+	'to' => 'Ue39b5f714f4424cb448ba4f6550bda5c',
+	'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result . "\r\n";
+}
 echo "No error";
